162d6e67facdaab2b2f3a32a0662edbc6e3da7c3
fmnihal <fattahmahmudnihal@gmail.com>
2025-05-13 12:55:36 +0600

    Done till step 7

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "http://localhost:3001";
+// const baseUrl = "http://localhost:3001";
+const baseUrl = "4602lab1webapp-eggyd9chbpc9gdet.eastasia-01.azurewebsites.net";
 
 export default baseUrl;
